8663c87b7d45211f7ac110ef39656c72f81355f3
elliot-putt-spectre <elliot.putt@agentsoftware.net>
2026-04-27 14:41:29 +0100

    chore: fix scaffold quality issues
    - Remove duplicate migration loading from TestCase setUp (ServiceProvider handles it via hasMigration)
    - Remove unimplemented AiUsage facade alias from composer.json extra.laravel
    - Remove unnecessary database/ PSR-4 autoload entry
    - Add .gitignore

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,12 +9,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-    }
-
     protected function getPackageProviders($app): array
     {
         return [
